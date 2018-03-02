63fb7b54df7bf4332c79a0d215fe2d0f0729b266
wangzhiyi <wangzhiyi@clickwise.cn>
2018-03-02 15:45:02 +0800

    add packages

--- util/device_attrs.py
+++ util/device_attrs.py
@@ -6,6 +6,7 @@
 import os
 import sys
 import pdb
+import time
 import random
 import json
 import traceback
@@ -15,6 +16,7 @@ from pyimei import ImeiSupport
 UAS = None
 
 def random_dev():
+    #获取随机设备信息
     imei = random_imei()
     ssid = random_wifi_name()
     imsi = random_imsi()
@@ -29,11 +31,151 @@ def random_dev():
             {"nettype":nettype},
             {"android_id":android_id}
     ]
-    ua = random_ua()
-    if ua != None:
-        for k,v in ua.iteritems():
-            d.append({k:v})
+    
+    build_probs = None
+    
+    while not build_probs:
+        builds = random_builds()
+        brand = builds["build_brand"]
+        model = builds["build_model"]
+
+        uas = get_brand_uas(brand,model)
+        if not uas:
+            print "Not found target ua for:"
+            print builds
+            pdb.set_trace()
+        build_probs = gen_build_probs(builds,uas)
+
+    packages = random_brand_packages(brand)
+    
+    for k,v in build_probs.iteritems():
+        d.append({k:v})
+    d.append({"packages":packages})
     return d
+
+def gen_build_probs(builds,uas):
+    bf = builds["build_fingerprint"]
+    android_version = bf.split("/")[2].split(":")[1]
+    good_ua = None
+    for ua in uas:
+        if ua["android_version"] == android_version:
+            good_ua = ua
+            break
+    if not good_ua:
+        return None
+    for k,v in good_ua.iteritems():
+        if builds.has_key(k):
+            continue
+        builds[k] = v
+    builds["os_build"] = builds["build_id"]
+    return builds
+
+def random_builds():
+    #随机生成builds信息
+    ret = {}
+    
+    fp = os.path.join(os.path.dirname(os.path.realpath(__file__)),'../data/builds.txt')
+    d_builds = {}
+    with open(fp,'r') as f:
+        d_builds = json.load(f)
+    builds = []
+    for k,v in d_builds.iteritems():
+        if k == "unkown":
+            continue
+
+        for k1,v1 in v.iteritems():
+            for item in v1:
+                builds.append(item)
+    return random.choice(builds)
+
+
+def random_brand_packages(brand):
+    #随机生成指定品牌的包列表
+    ret = {}
+    ret["sys"] = []
+    ret["brand"] = []
+    ret["third"] = []
+    fp = os.path.join(os.path.dirname(os.path.realpath(__file__)),'../data/packages.txt')
+    d_packages = {}
+    with open(fp,'r') as f:
+        cnt = f.read()
+        d_packages = eval(cnt)
+    #手机出厂时间生成
+    t_area = random.randint(0,100)
+    if t_area < 30:
+        t_create = int(time.time()-random.randint(20*24*3600,6*30*24*3600))*1000
+    if t_area >= 30 and t_area < 60:
+        t_create = int(time.time()-random.randint(6*30*24*3600,12*30*24*3600))*1000
+    if t_area >= 60 and t_area < 80:
+        t_create = int(time.time()-random.randint(12*30*24*3600,2*12*30*24*3600))*1000
+    if t_area >= 80:
+        t_create = int(time.time()-random.randint(2*12*30*24*3600,5*12*30*24*3600))*1000
+    
+    pk_sys = d_packages["sys"][brand]
+    for k in pk_sys.keys():
+        pk_box = pk_sys[k]
+        rd = random.randint(0,100)
+        if rd < 80:
+            pk = random.choice(pk_box)
+            pk["packageName"] = k
+            pk["firstInstallTime"] = '%d' % t_create
+            pk["lastUpdateTime"] = '%d' % t_create
+            ret["sys"].append(pk)
+    
+    pk_brand = d_packages["brand"][brand]
+    for k in pk_brand:
+        pk_box = pk_brand[k]
+        rd = random.randint(0,100)
+        if rd < 80:
+            t_dis = random.randint(2*24*3600,12*30*24*3600)
+            t_install = (time.time()-t_dis)*1000 
+            t_up = random.randint(0,100)
+            if t_up < 5:
+                t_update = (time.time()-random.randint(2*24*3600,10*24*3600))*1000
+            else:
+                t_update = t_install
+            t_update = max(t_create,t_update)
+            pk = random.choice(pk_box)
+            pk["packageName"] = k
+            pk["firstInstallTime"] = '%d' % t_install
+            pk["lastUpdateTime"] = '%d' % t_update
+            ret["brand"].append(pk)
+    
+    rd = random.randint(0,100)
+    if rd < 5:
+        app_c = random.randint(1,10)
+    elif rd <30:
+        app_c = random.randint(10,15)
+    elif rd<55:
+        app_c = random.randint(15,20)
+    elif rd<90:
+        app_c = random.randint(20,25)
+    else:
+        app_c = random.randint(25,40)
+    hit_p = app_c*100/40
+    
+    pk_third = d_packages["third"]
+    for k in pk_third:
+        lst_pk = pk_third[k]
+        rd_hit = random.randint(0,100)
+        if rd_hit < hit_p:
+            t_dis = random.randint(2*24*3600,12*30*24*3600)
+            t_install = (time.time()-t_dis)*1000 
+            t_up = random.randint(0,100)
+            if t_up < 10:
+                print t_up
+                t_update = (time.time()-random.randint(2*24*3600,10*24*3600))*1000
+                t_update = max(t_create,t_update)
+            else:
+                t_update = t_install
+            pk = random.choice(lst_pk)
+            pk["packageName"] = k
+            pk["firstInstallTime"] = '%d' % t_install
+            pk["lastUpdateTime"] = '%d' % t_update
+            ret["third"].append(pk)
+            print pk
+
+    return ret
     
 def random_wifi_name():
     seed = '0123456789qwertyuiopasdfghjklzxcvbnm'
@@ -72,21 +214,45 @@ def random_android_id():
 
 def random_ua():
     global UAS
-    if not UAS:
-        fp = os.path.join(os.path.dirname(os.path.realpath(__file__)),'../data/uas.txt')
-        f = open(fp,'r')
-        UAS = []
-        lines = f.readlines()
-        for line in lines:
-            try:
-                UAS.append(eval(line.strip("\n")))
-            except Exception as e:
-                traceback.print_exc()
+    #if not UAS:
+    fp = os.path.join(os.path.dirname(os.path.realpath(__file__)),'../data/uas.txt')
+    f = open(fp,'r')
+    UAS = []
+    lines = f.readlines()
+    for line in lines:
+        try:
+            UAS.append(eval(line.strip("\n")))
+        except Exception as e:
+            traceback.print_exc()
     n = len(UAS)
     if n == 0:
         return None
     return random.choice(UAS)
 
+def get_brand_uas(brand,model):
+    fp = os.path.join(os.path.dirname(os.path.realpath(__file__)),'../data/uas.txt')
+    f = open(fp,'r')
+    uas = []
+    lines = f.readlines()
+    for line in lines:
+        try:
+            uas.append(eval(line.strip("\n")))
+        except Exception as e:
+            traceback.print_exc()
+    lst_dev = []
+    for dev in uas:
+        if dev["android_model"].lower() == model.lower():
+            lst_dev.append(dev)
+    if len(lst_dev) == 0:
+        return None
+    return lst_dev
+
+    '''
+    if len(lst_dev) > 0:
+        print "Warning->get_brand_ua:Not uique device selected!"
+        print lst_dev
+    return random.choice(lst_dev)
+    '''
 
 if __name__ == "__main__":
     print random_dev()
